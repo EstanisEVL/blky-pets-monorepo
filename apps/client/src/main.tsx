c7948f45e45f295d16ebdcba83514901b9f47cd1
EstanisEVL <estanislaovl@gmail.com>
2024-01-16 11:13:46 -0300

    fix: Comment out UserDataProvider from main file

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { UserDataProvider } from "./components/providers/UserDataProvider.tsx";
-// import { ProductsProvider } from "./components/providers/ProductsProvider.tsx";
+// import { UserDataProvider } from "./components/providers/UserDataProvider.tsx";
+import { ProductsProvider } from "./components/providers/ProductsProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <ProductsProvider> */}
-    <UserDataProvider>
+    <ProductsProvider>
+      {/* <UserDataProvider> */}
       <App />
-    </UserDataProvider>
-    {/* </ProductsProvider> */}
+      {/* </UserDataProvider> */}
+    </ProductsProvider>
   </React.StrictMode>
 );
