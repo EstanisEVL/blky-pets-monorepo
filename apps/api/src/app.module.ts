4b5b421519960e65f2cd4b789eb8871e1e526687
EstanisEVL <estanislaovl@gmail.com>
2024-02-11 17:31:22 -0300

    fix: Add vercel config for monorepo upload

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -22,6 +22,7 @@ import { join } from 'path';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'client', 'dist'),
+      exclude: ['api/*'],
     }),
     ConfigModule.forRoot({
       envFilePath: `${process.cwd()}/.env.${
